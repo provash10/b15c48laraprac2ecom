af34cda4d7a3f4ce8766f77cfecc1a85e9e3ecad
provash10 <provash20cb@gmail.com>
2025-05-15 15:58:05 +0600

    50L3 FrontendController index blade

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,9 @@
 <?php
 
-use App\Http\Controllers\HomeController;
+use App\Http\Controllers\FrontendController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+Route::get('/',[FrontendController::class,'index']);
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
 
-Route::get('/',[HomeController::class, 'index']);
-Route::get('/contact-us',[HomeController::class, 'contact']);
+
